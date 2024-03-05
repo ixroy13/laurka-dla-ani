751132c60553db33e2c761fc9052737d6d7e2770
Aleksander Pal <102484538+ixroy13@users.noreply.github.com>
2024-03-05 12:51:18 +0100

    Month calculation bug fix

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const buttons = document.querySelectorAll(".welcome button");
 const welcomeSection = document.querySelector(".welcome");
 const monthParagraph = document.querySelector("p.months");
 const dayParagraph = document.querySelector("p.days");
-const hourParagraph = document.querySelector("p.hours")
-const minuteParagraph = document.querySelector("p.minutes")
+const hourParagraph = document.querySelector("p.hours");
+const minuteParagraph = document.querySelector("p.minutes");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -13,8 +13,8 @@ buttons.forEach((button) => {
 });
 
 buttons[0].addEventListener("click", () => {
-  document.querySelector("img.ice").classList.add("show")
-})
+  document.querySelector("img.ice").classList.add("show");
+});
 
 buttons[1].addEventListener("click", () => {
   document.querySelector("img.sing").classList.add("show");
@@ -26,20 +26,35 @@ buttons[2].addEventListener("click", () => {
 
 function timeUpdate() {
   let date = new Date();
-  // let date = new Date("2012-02-11T09:00:00")
+  // let date = new Date("2024-03-05T09:01:01");
   let birthday = new Date("2008-02-04T09:00:00");
   let timeDifference = date - birthday;
   let years = Math.floor(timeDifference / (365.25 * 24 * 60 * 60 * 1000));
-  let months = Math.floor((timeDifference % (365.25 * 24 * 60 * 60 * 1000)) /(30.44 * 24 * 60 * 60 * 1000));
-  let days = Math.floor((timeDifference % (30.435 * 24 * 60 * 60 * 1000)) /(24 * 60 * 60 * 1000));
-  let hours = Math.floor((timeDifference % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+  let months = Math.floor(
+    (timeDifference % (365.25 * 24 * 60 * 60 * 1000)) /
+      (30 * 24 * 60 * 60 * 1000)
+  );
+  let days = Math.floor(
+    (timeDifference % (30.435 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)
+  );
+  let hours = Math.floor(
+    (timeDifference % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+  );
   let minutes = Math.floor((timeDifference % (60 * 60 * 1000)) / (60 * 1000));
   let seconds = Math.floor((timeDifference % (60 * 1000)) / 1000);
 
-  months == 0 ? monthParagraph.style.display = "none" : monthParagraph.style.display = "block"
-  days == 0 ? dayParagraph.style.display = "none" : dayParagraph.style.display = "block"
-  hours == 0 ? hourParagraph.style.display = "none" : hourParagraph.style.display = "block"
-  minutes == 0 ? minuteParagraph.style.display = "none" : minuteParagraph.style.display = "block"
+  months == 0
+    ? (monthParagraph.style.display = "none")
+    : (monthParagraph.style.display = "block");
+  days == 0
+    ? (dayParagraph.style.display = "none")
+    : (dayParagraph.style.display = "block");
+  hours == 0
+    ? (hourParagraph.style.display = "none")
+    : (hourParagraph.style.display = "block");
+  minutes == 0
+    ? (minuteParagraph.style.display = "none")
+    : (minuteParagraph.style.display = "block");
 
   let monthWord;
   let dayWord;
@@ -47,32 +62,32 @@ function timeUpdate() {
   let minutesWord;
   let secondWord;
 
-  if(months == 1) {
-    monthWord = "miesiąc"
+  if (months == 1) {
+    monthWord = "miesiąc";
   } else if (months < 4 && months > 0) {
-    monthWord = "miesiące"
+    monthWord = "miesiące";
   } else {
-    monthWord = "miesięcy"
+    monthWord = "miesięcy";
   }
 
-  if(days == 1) {
-    dayWord = "dzień"
+  if (days == 1) {
+    dayWord = "dzień";
   } else {
-    dayWord = "dni"
+    dayWord = "dni";
   }
 
-  if(hours == 1) {
-    hourWord = "godzinę"
+  if (hours == 1) {
+    hourWord = "godzinę";
   } else if ((hours < 5 && hours > 0) || hours > 21) {
-    hourWord = "godziny"
+    hourWord = "godziny";
   } else {
-    hourWord = "godzin"
+    hourWord = "godzin";
   }
 
   if (minutes == 1) {
     minutesWord = "minutę";
   } else if (
-    (minutes > 0 && minutes < 5) || 
+    (minutes > 0 && minutes < 5) ||
     (minutes > 21 && minutes < 25) ||
     (minutes > 31 && minutes < 35) ||
     (minutes > 41 && minutes < 45) ||
@@ -86,7 +101,7 @@ function timeUpdate() {
   if (seconds == 1) {
     secondWord = "sekundę";
   } else if (
-    (seconds < 5) ||
+    seconds < 5 ||
     (seconds > 21 && seconds < 25) ||
     (seconds > 31 && seconds < 35) ||
     (seconds > 41 && seconds < 45) ||
@@ -101,9 +116,18 @@ function timeUpdate() {
   document.querySelector("p.months").innerText = `${months + " " + monthWord}`;
   document.querySelector("p.days").innerText = `${days + " " + dayWord}`;
   document.querySelector("p.hours").innerText = `${hours + " " + hourWord}`;
-  document.querySelector("p.minutes").innerText = `${minutes + " " + minutesWord}`;
-  document.querySelector("p.seconds").innerText = `${seconds == 0 ? "" : seconds + " " + secondWord} temu`;
+  document.querySelector("p.minutes").innerText = `${
+    minutes + " " + minutesWord
+  }`;
+  document.querySelector("p.seconds").innerText = `${
+    seconds == 0 ? "" : seconds + " " + secondWord
+  } temu`;
+
+  let secondsProgress = Math.ceil((seconds / 60) * 100);
+  let minutesProgress = Math.ceil((minutes / 60) * 100);
+  let hoursProgress = Math.ceil((hours / 24) * 100);
+  let daysProgress = Math.ceil((days / 30.25) * 100);
 }
 
-setInterval(timeUpdate, 500);
+setInterval(timeUpdate, 300);
 timeUpdate();
